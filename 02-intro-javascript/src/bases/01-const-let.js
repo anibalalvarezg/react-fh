59e566df651263206f82a9d6c49a1ad7ddaa82e6
Aníbal Álvarez <anibal.alvarezg@gmail.com>
2021-02-15 23:48:00 -0300

    Intro javascript 01

---
+++ 02-intro-javascript/src/bases/01-const-let.js
@@ -0,0 +1,16 @@
+const name = 'Anibal';
+const lastname = 'Alvarez'; 
+
+
+let giveValue = 5;
+giveValue = 4; 
+
+console.log( name, lastname, giveValue);
+
+
+if (true) {
+    let giveValue = 6; 
+    console.log(giveValue);
+}
+
+console.log(giveValue);
