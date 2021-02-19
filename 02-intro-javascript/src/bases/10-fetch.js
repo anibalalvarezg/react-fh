d6e0591d5a3a589a00631e21b2437e6219147c5e
Aníbal Álvarez <anibal.alvarezg@gmail.com>
2021-02-18 22:26:39 -0300

    09 Fetch

---
+++ 02-intro-javascript/src/bases/10-fetch.js
@@ -0,0 +1,13 @@
+const apiKey = '8ghzCIKOoxCuqerFzlUCD1PF3136sdGY';
+
+const httpRequest = fetch(`http://api.giphy.com/v1/gifs/random?api_key=${ apiKey }`);
+
+httpRequest
+    .then( resp => resp.json())
+    .then( ({data}) => {
+        const { url } = data.images.original;
+        const img = document.createElement('img');
+        img.src = url;
+        document.body.append(img);
+    })
+    .catch( console.warn );
