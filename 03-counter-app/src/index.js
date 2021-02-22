7a99b1a40132b1c686fab8fdecb7afd2e647b544
Aníbal Álvarez <anibal.alvarezg@gmail.com>
2021-02-21 21:42:04 -0300

    03 Hello World

---
+++ 03-counter-app/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const greetings = <h1>Hello World!</h1>;
+const divRoot = document.querySelector('#root');
+
+ReactDOM.render(greetings, divRoot);
