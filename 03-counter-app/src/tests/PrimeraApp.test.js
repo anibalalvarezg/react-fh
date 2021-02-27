59aa14cddb14c4e74eb8dbeed56af88abdc5b0c9
Aníbal Álvarez <anibal.alvarezg@gmail.com>
2021-02-26 22:50:45 -0300

    Test with enzyme snapshots

--- 03-counter-app/src/tests/PrimeraApp.test.js
+++ 03-counter-app/src/tests/PrimeraApp.test.js
@@ -1,14 +1,22 @@
 import React from 'react'
-import { render } from '@testing-library/react';
+import '@testing-library/jest-dom';
 import { PrimeraApp } from '../PrimeraApp';
+import { shallow } from 'enzyme';
 
 describe('Test PrimeraApp component', () => {
-    test('should show message "Hola mundo"', () => {
-        const greeting = "Hola mundo"; 
+    // test('should show message "Hola mundo"', () => {
+    //     const greeting = "Hola mundo"; 
 
-        const { getByText } = render(<PrimeraApp saludo={greeting} subtitulo="Test" />);
+    //     const { getByText } = render(<PrimeraApp saludo={greeting} subtitulo="Test" />);
 
-        expect(getByText(greeting)).toBeInTheDocument(); 
-    }); 
+    //     expect(getByText(greeting)).toBeInTheDocument(); 
+    // }); 
 
+    test('should show PrimeraApp correctly', () => {
+        const greeting = 'Hola mundo';
+        const wrapper = shallow( <PrimeraApp saludo={greeting} subtitulo="Esto es una prueba" />);
+
+        expect( wrapper ).toMatchSnapshot()
+    })
+    
 });
