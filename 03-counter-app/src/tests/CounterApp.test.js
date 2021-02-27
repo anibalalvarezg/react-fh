52dd4509c2f9216540ab07d50b2e32b553cb3cfc
Aníbal Álvarez <anibal.alvarezg@gmail.com>
2021-02-26 23:45:17 -0300

    Click tests

--- 03-counter-app/src/tests/CounterApp.test.js
+++ 03-counter-app/src/tests/CounterApp.test.js
@@ -3,16 +3,50 @@ import { shallow } from 'enzyme';
 import CounterApp from '../CounterApp';
 
 describe('Tests CounterApp component', () => {
-    test('should show CounterApp correctly', () => {
-        const wrapper = shallow(<CounterApp />); 
-        expect( wrapper ).toMatchSnapshot();
+    let wrapper = shallow(<CounterApp value={100} />);
+
+    beforeEach(() => {
+        wrapper = shallow(<CounterApp value={100} />);
     });
-    
+    test('should show CounterApp correctly', () => {
+        expect(wrapper).toMatchSnapshot();
+    });
+
     test('should show value by default 100', () => {
-        const wrapper = shallow( <CounterApp value={100}/>);
         const counterText = wrapper.find('p').text().trim();
 
-        expect(counterText).toBe('100'); 
+        expect(counterText).toBe('100');
     });
-    
+
+    test('should increment with the button plus one', () => {
+        const buttonAdd = wrapper.find('button').at(0).simulate('click', {});
+        const counterText = wrapper.find('p').text().trim();
+
+        expect(counterText).toBe('101');
+    });
+
+    test('should increment with the button subtract one', () => {
+        const buttonSubtract = wrapper.find('button').at(2).simulate('click', {});
+        const counterText = wrapper.find('p').text().trim();
+
+        expect(counterText).toBe('100');
+    });
+
+    test('should click reset button and set default value', () => {
+        const wrapper = shallow(<CounterApp value={100} />);
+
+        wrapper.find('button').at(0).simulate('click', {});
+        wrapper.find('button').at(0).simulate('click', {});
+        wrapper.find('button').at(0).simulate('click', {});
+
+        wrapper.find('button').at(1).simulate('click', {});
+
+        const counterText = wrapper.find('p').text().trim();
+        console.log(counterText);
+
+        expect(counterText).toBe('102');
+
+    })
+
+
 });
