6c0c57c34d6157e81fb841673caac001723f57dd
Aníbal Álvarez <anibal.alvarezg@gmail.com>
2021-03-03 23:29:13 -0300

    Hooks - useState and useEffect

---
+++ 05-hook-app/src/components/01-useState/CounterWithCustomHook.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { useCounter } from '../../hooks/useCounter';
+import './Counter.css';
+
+
+export const CounterWithCustomHook = () => {
+
+    const {state, increment, decrement, reset } = useCounter(100);
+
+    return (
+        <>
+            <h1>Counter with Hook: { state }</h1>
+
+            <hr />
+            
+            <button className="btn btn-primary" onClick={ () => increment(2) }>+1</button>
+            <button className="btn btn-secondary mx-3" onClick={ reset }>Reset</button>
+            <button className="btn btn-primary" onClick={ () => decrement(2) }>-1</button>
+        </>
+    )
+}
--- 05-hook-app/src/index.js
+++ 05-hook-app/src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HookApp } from './HookApp';
+// import { CounterApp } from './components/01-useState/CounterApp';
+// import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook';
+import { SimpleForm } from './components/02-useEffect/SimpleForm';
+// import { HookApp } from './HookApp';
 
 ReactDOM.render(
-  <HookApp />,
+  <SimpleForm />,
   document.getElementById('root')
 );
